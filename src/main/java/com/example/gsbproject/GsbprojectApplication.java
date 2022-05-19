b5f77ce8201a62534a9b535773556e5baa6813dc
ssg4262 <rhtmdqja275@naver.com>
2022-05-19 21:02:42 +0900

    5-19 first

---
+++ src/main/java/com/example/gsbproject/GsbprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.gsbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsbprojectApplication.class, args);}
+
+}
---
+++ src/test/java/com/example/gsbproject/GsbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
